2ed2f167f51b7e495181736c6242615e21fb25f7
AliaksandrPosakh <arsenev_alesha@mail.ru>
2018-10-07 22:26:39 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+
+  const simpleList = getNum(base);
+let initObj = {};
+
+simpleList.forEach(el => {
+if(initObj[el] == null) {
+initObj[el] = 1;
+} else {
+const newCount = initObj[el] + 1;
+initObj[el] = newCount;
+}
+})
+const uniqueItems = Array.from(new Set(simpleList));
+
+let resNum = uniqueItems.map(el => Math.floor(maxPowerOf(el, number) / initObj[el]));
+const sorted = resNum.sort((a, b) => a-b);
+
+return sorted[0]; 
+}
+
+function getNum(x, arr = [], num = 2) { //get simple multiplier
+if (x === '1') {
+arr.push(1);
+}
+if(x % num === 0){
+arr.push(num);
+x = x / num;
+
+arr = getNum(x, arr, num); 
+
+} else if(x % num !== 0){
+if(!(x <= num)){
+num++;
+arr = getNum(x, arr, num);
+}
+}
+return arr;
+}
+
+function maxPowerOf(p, n) { // степень k простого числа p, на которую делится n!
+let k = 0;
+let powerOfP = 1;
+while (true)
+{
+powerOfP *= p; 
+let addend = Math.floor(n / powerOfP);
+if (addend == 0)
+return k;
+k += addend;
+}
 }
